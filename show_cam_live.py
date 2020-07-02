89c28369f0f917ed5d2b6802df1367e233eb374b
Nicklas Behler <nicklas.behler@stud.tu-darmstadt.de>
2020-07-02 12:40:27 +0200

    GoPro stuff

--- show_cam_live.py
+++ show_cam_live.py
@@ -3,17 +3,19 @@ import tensorflow as tf
 import numpy as np
 import os
 import pandas as pd
-from keras_retinanet import models
-from keras_retinanet.utils.image import read_image_bgr, preprocess_image, resize_image
-from keras_retinanet.utils.visualization import draw_box, draw_caption
-from keras_retinanet.utils.colors import label_color
+from goprocam import GoProCamera
+from goprocam import constants
 
+# cascPath = "/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml"
+# faceCascade = cv2.CascadeClassifier(cascPath)
+# gpCam = GoProCamera.GoPro()
+# gpCam.stream('udp://127.0.0.1:10000')
+# cap = cv2.VideoCapture("udp://127.0.0.1:10000")
 THRESH_SCORE = 0.6
 classifier_model = tf.keras.models.load_model('models/CardClassifier.h5')
 bbox_model = tf.keras.models.load_model('models/BoundingBox_Locator.h5',
                                         custom_objects={'leaky_relu': tf.nn.leaky_relu})
-model = models.load_model('models/resnet50_csv_10.h5', backbone_name='resnet50')
-model = models.convert_model(model)
+scale_factor = 0
 labels_to_names = pd.read_csv(
   'classes.csv',
   header=None,
@@ -23,13 +25,23 @@ labels_to_names = pd.read_csv(
 
 def show_webcam(mirror=False):
     cam = cv2.VideoCapture(1)   # 0 for build in cam, 1 for usb cam
+    if not cam.isOpened():
+        print('Could not open video')
+        return
     while True:
         ret_val, img = cam.read()
         if mirror:
             img = cv2.flip(img, 1)
-        bboxes, scores, classes = predict_box_resnet(img)
-        img = draw_bounding_box(img, bboxes)
-        img = show_class(img, classes, bboxes)
+        # bboxes, classes = predict_box(img)
+        # for box in bboxes:
+        #     box[0][0] = box[0][0] * scale_factor[1]
+        #     box[0][1] = box[0][1] * scale_factor[2]
+        #     box[0][2] = box[0][2] * scale_factor[1]
+        #     box[0][3] = box[0][3] * scale_factor[2]
+        # img = draw_bounding_box(img, bboxes)
+        # img = show_class(img, classes, bboxes)
+        # print(np.shape(img))
+
         cv2.imshow('my webcam', img)
         if cv2.waitKey(1) == 27:
             break  # esc to quit
@@ -39,8 +51,11 @@ def show_webcam(mirror=False):
 def predict_labels(img, boxes):
     img = img[0]
     labels = []
-    for i, box in enumerate(boxes[0]):
-        crop = img[int(box[1]):int(box[3]), int(box[0]):int(box[2])]
+    for i, box in enumerate(boxes):
+        if any(x < 10 for x in box[0]):
+            continue
+        #print(box[0])
+        crop = img[int(box[0][1]):int(box[0][3]), int(box[0][0]):int(box[0][2])]
         crop = cv2.resize(crop, (80, 80), interpolation = cv2.INTER_AREA)
         gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
 
@@ -57,7 +72,11 @@ def predict_labels(img, boxes):
 
 
 def predict_box(image):
+    global scale_factor
+    orig_shape = np.shape(image)
     image = cv2.resize(image, (135, 135))
+    resized_image_shape = np.shape(image)
+    scale_factor = np.flip(np.divide(orig_shape, resized_image_shape))
     image = np.expand_dims(image, axis=0)
     boxes = bbox_model.predict(image)
 
@@ -90,34 +109,52 @@ def predict_labels_resnet(img, boxes, scores):
   return labels
 
 
-def predict_box_resnet(image):
-    image = preprocess_image(image.copy())
-    image, scale = resize_image(image)
-    image = np.expand_dims(image, axis=0)
-    boxes, scores, labels = model.predict_on_batch(
-        image
-    )
-
-    labels = predict_labels_resnet(image, boxes, scores)
-    boxes /= scale
-    return boxes, scores, labels
+# def predict_box_resnet(image):
+#     image = preprocess_image(image.copy())
+#     image, scale = resize_image(image)
+#     image = np.expand_dims(image, axis=0)
+#     #boxes, scores, labels = model.predict_on_batch(image)
+#
+#     labels = predict_labels_resnet(image, boxes, scores)
+#     boxes /= scale
+#     return boxes, scores, labels
 
 
 def show_class(image, cards, bboxes):
     for i in range(len(cards)):
-        cv2.putText(image, cards[i], (round(bboxes[0][i][0]), round(bboxes[0][i][1])), cv2.FONT_HERSHEY_COMPLEX,
+        cv2.putText(image, cards[i], (round(bboxes[i][0][0]), round(bboxes[i][0][1])), cv2.FONT_HERSHEY_COMPLEX,
                     1, (255, 255, 255), 2)
     return image
 
 
 def draw_bounding_box(image, bboxes):
-    for bbox in bboxes[0]:
-        image = cv2.rectangle(image, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 255, 0), 2)
+    for bbox in bboxes:
+        image = cv2.rectangle(image, (bbox[0][0], bbox[0][1]), (bbox[0][2], bbox[0][3]), (255, 0, 0), 2)
     return image
 
 
+def predict_image(filepath):
+    global scale_factor
+    img = cv2.imread(filepath)
+    boxes, classes = predict_box(img)
+    print(scale_factor)
+    for box in boxes:
+        box[0][0] = box[0][0]*scale_factor[1]
+        box[0][1] = box[0][1]*scale_factor[2]
+        box[0][2] = box[0][2]*scale_factor[1]
+        box[0][3] = box[0][3]*scale_factor[2]
+    img = show_class(img, classes, boxes)
+    img = draw_bounding_box(img, boxes)
+    #img = cv2.rectangle(img, (50, 320), (92, 333), (255, 0, 0), -1)
+    print(boxes)
+    print(classes)
+    cv2.imshow('image', img)
+    cv2.waitKey()
+
+
 def main():
-    show_webcam(mirror=True)
+    show_webcam(mirror=False)
+    #predict_image('test.png')
 
 
 if __name__ == '__main__':
