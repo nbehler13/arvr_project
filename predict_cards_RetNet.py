ecef7a3c13ffb891d49c332207b9dccf4826ebee
Sicbe <marcello.pro@t-online.de>
2020-07-07 10:04:51 +0200

    Added retinanet (trained model is missing)

---
+++ predict_cards_RetNet.py
@@ -0,0 +1,96 @@
+import cv2
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import threading
+import sys
+import matplotlib.pyplot as plt
+
+#sys.path.append('/keras-retinanet-master/')
+from keras_retinanet_master.keras_retinanet import models as retmodels
+from keras_retinanet_master.keras_retinanet.utils.image import read_image_bgr, preprocess_image, resize_image
+from keras_retinanet_master.keras_retinanet.utils.visualization import draw_box, draw_caption
+
+THRESH_SCORE = 0.6
+classifier_model = tf.keras.models.load_model('models/CardClassifier.h5')
+
+pretrained_path = 'models/resnet50_own_dataset_20.h5'
+RetNet_model = retmodels.load_model(pretrained_path, backbone_name='resnet50')
+RetNet_model = retmodels.convert_model(RetNet_model)
+
+labels_to_names = pd.read_csv(
+  'classes.csv',
+  header=None,
+  index_col=0
+).to_dict()[1]
+
+scale_factor = 0
+
+THRES_SCORE = 0.6
+def draw_detections(image, boxes, labels):
+    if boxes == []:
+        return
+    for box, label in zip(boxes[0], labels):
+        color = [0, 255, 0]
+        b = box.astype(int)
+        draw_box(image, b, color=color)
+
+        caption = "{}".format(label)
+        draw_caption(image, b, caption)
+
+
+def predict_labels_resnet(img, boxes, scores):
+    img = img[0]
+    labels = []
+    for i, box in enumerate(boxes[0]):
+        if scores[0][i] < THRES_SCORE:
+            break
+        crop = img[int(box[1]):int(box[3]), int(box[0]):int(box[2])]
+        crop = cv2.resize(crop, (80, 80), interpolation = cv2.INTER_AREA)
+        gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
+
+        crop = cv2.Canny(np.uint8(gray), 30, 200)
+        crop = crop/255.0
+        crop = crop.reshape(-1, 80, 80, 1)
+        prediction = classifier_model.predict(crop)
+        idx = np.argmax(prediction[0])
+        for key, value in labels_to_names.items():
+            if value == idx:
+                labels.append(key)
+                break
+    
+    boxes = boxes[:,:len(labels),:]
+    return boxes, labels
+
+
+def predict_box_resnet(image):
+    image = preprocess_image(image.copy())
+    image, scale = resize_image(image)
+    image = np.expand_dims(image, axis=0)
+    boxes, scores, labels = RetNet_model.predict_on_batch(image)
+
+    boxes, labels = predict_labels_resnet(image, boxes, scores)
+    boxes /= scale
+    return boxes, labels
+
+
+class predictThread():
+   def __init__(self):
+        self.boxes = []
+        self.labels = []
+
+   def run(self, img):
+        self.boxes, self.labels = predict_box_resnet(img)
+
+
+
+def predict_image(filepath):
+    global scale_factor
+    img = cv2.imread(filepath)
+    boxes, classes = predict_box_resnet(img)
+    draw = img.copy()
+    draw_detections(draw, boxes, classes)
+    print(boxes)
+    print(classes)
+    cv2.imshow('image', draw)
+    cv2.waitKey()
--- show_cam_live.py
+++ show_cam_live.py
@@ -2,154 +2,56 @@ import cv2
 import tensorflow as tf
 import numpy as np
 import os
-import pandas as pd
-from goprocam import GoProCamera
-from goprocam import constants
+#import pandas as pd
+from threading import Thread
+import queue
+import threading
+from predict_cards import predict_box, show_class, draw_bounding_box, predictThread # , predict_image
+from predict_cards_RetNet import draw_detections, predict_image #, predictThread
+
+# For using RetinaNet bounding box detector:
+# - import predictThread from predict_cards_RetNet
+# - comment out: img = draw_bounding_box(img, bboxes)
+# - comment out: img = show_class(img, classes, bboxes)
+# - comment in: draw_detections(img, bboxes, classes)
+
+#from goprocam import GoProCamera
+#from goprocam import constants
 
 # cascPath = "/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml"
 # faceCascade = cv2.CascadeClassifier(cascPath)
 # gpCam = GoProCamera.GoPro()
 # gpCam.stream('udp://127.0.0.1:10000')
 # cap = cv2.VideoCapture("udp://127.0.0.1:10000")
-THRESH_SCORE = 0.6
-classifier_model = tf.keras.models.load_model('models/CardClassifier.h5')
-bbox_model = tf.keras.models.load_model('models/BoundingBox_Locator.h5',
-                                        custom_objects={'leaky_relu': tf.nn.leaky_relu})
-scale_factor = 0
-labels_to_names = pd.read_csv(
-  'classes.csv',
-  header=None,
-  index_col=0
-).to_dict()[1]
-
 
 def show_webcam(mirror=False):
-    cam = cv2.VideoCapture(1)   # 0 for build in cam, 1 for usb cam
+    cam = cv2.VideoCapture(0)   # 0 for build in cam, 1 for usb cam
     if not cam.isOpened():
         print('Could not open video')
         return
+    frame = 200
+    pred_thread = predictThread()
     while True:
         ret_val, img = cam.read()
         if mirror:
             img = cv2.flip(img, 1)
-        # bboxes, classes = predict_box(img)
-        # for box in bboxes:
-        #     box[0][0] = box[0][0] * scale_factor[1]
-        #     box[0][1] = box[0][1] * scale_factor[2]
-        #     box[0][2] = box[0][2] * scale_factor[1]
-        #     box[0][3] = box[0][3] * scale_factor[2]
-        # img = draw_bounding_box(img, bboxes)
-        # img = show_class(img, classes, bboxes)
-        # print(np.shape(img))
+
+        if frame > 100:
+            t = Thread(target=pred_thread.run, args=(img,))
+            t.start()
+            print("new prediction")
+            frame = 0
+        bboxes = pred_thread.boxes
+        classes = pred_thread.labels
+        img = draw_bounding_box(img, bboxes)
+        img = show_class(img, classes, bboxes)
+        #draw_detections(img, bboxes, classes)
+        frame += 1
 
         cv2.imshow('my webcam', img)
         if cv2.waitKey(1) == 27:
             break  # esc to quit
     cv2.destroyAllWindows()
-
-
-def predict_labels(img, boxes):
-    img = img[0]
-    labels = []
-    for i, box in enumerate(boxes):
-        if any(x < 10 for x in box[0]):
-            continue
-        #print(box[0])
-        crop = img[int(box[0][1]):int(box[0][3]), int(box[0][0]):int(box[0][2])]
-        crop = cv2.resize(crop, (80, 80), interpolation = cv2.INTER_AREA)
-        gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
-
-        crop = cv2.Canny(np.uint8(gray), 30, 200)
-        crop = crop/255.0
-        crop = crop.reshape(-1, 80, 80, 1)
-        prediction = classifier_model.predict(crop)
-        idx = np.argmax(prediction[0])
-        for key, value in labels_to_names.items():
-            if value == idx:
-                labels.append(key)
-                break
-    return labels
-
-
-def predict_box(image):
-    global scale_factor
-    orig_shape = np.shape(image)
-    image = cv2.resize(image, (135, 135))
-    resized_image_shape = np.shape(image)
-    scale_factor = np.flip(np.divide(orig_shape, resized_image_shape))
-    image = np.expand_dims(image, axis=0)
-    boxes = bbox_model.predict(image)
-
-    labels = predict_labels(image, boxes)
-    #boxes /= scale
-    return boxes, labels
-
-
-THRES_SCORE = 0.6
-def predict_labels_resnet(img, boxes, scores):
-  img = img[0]
-  labels = []
-  for i, box in enumerate(boxes[0]):
-    if scores[0][i] < THRES_SCORE:
-      break
-    crop = img[int(box[1]):int(box[3]), int(box[0]):int(box[2])]
-    crop = cv2.resize(crop, (80, 80), interpolation = cv2.INTER_AREA)
-    gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
-
-    crop = cv2.Canny(np.uint8(gray), 30, 200)
-    crop = crop/255.0
-    crop = crop.reshape(-1, 80, 80, 1)
-    prediction = classifier_model.predict(crop)
-    idx = np.argmax(prediction[0])
-    for key, value in labels_to_names.items():
-      if value == idx:
-        labels.append(key)
-        break
-
-  return labels
-
-
-# def predict_box_resnet(image):
-#     image = preprocess_image(image.copy())
-#     image, scale = resize_image(image)
-#     image = np.expand_dims(image, axis=0)
-#     #boxes, scores, labels = model.predict_on_batch(image)
-#
-#     labels = predict_labels_resnet(image, boxes, scores)
-#     boxes /= scale
-#     return boxes, scores, labels
-
-
-def show_class(image, cards, bboxes):
-    for i in range(len(cards)):
-        cv2.putText(image, cards[i], (round(bboxes[i][0][0]), round(bboxes[i][0][1])), cv2.FONT_HERSHEY_COMPLEX,
-                    1, (255, 255, 255), 2)
-    return image
-
-
-def draw_bounding_box(image, bboxes):
-    for bbox in bboxes:
-        image = cv2.rectangle(image, (bbox[0][0], bbox[0][1]), (bbox[0][2], bbox[0][3]), (255, 0, 0), 2)
-    return image
-
-
-def predict_image(filepath):
-    global scale_factor
-    img = cv2.imread(filepath)
-    boxes, classes = predict_box(img)
-    print(scale_factor)
-    for box in boxes:
-        box[0][0] = box[0][0]*scale_factor[1]
-        box[0][1] = box[0][1]*scale_factor[2]
-        box[0][2] = box[0][2]*scale_factor[1]
-        box[0][3] = box[0][3]*scale_factor[2]
-    img = show_class(img, classes, boxes)
-    img = draw_bounding_box(img, boxes)
-    #img = cv2.rectangle(img, (50, 320), (92, 333), (255, 0, 0), -1)
-    print(boxes)
-    print(classes)
-    cv2.imshow('image', img)
-    cv2.waitKey()
 
 
 def main():
